95fdca124e56d36b5972b2ba9a437cfe73590e44
Jussipol <83532208+JussiP-code@users.noreply.github.com>
2023-03-02 19:58:41 +0200

    fontchange

--- components/MyAppText/MyAppText.js
+++ components/MyAppText/MyAppText.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, Platform} from 'react-native';
 
 export const MyAppText = ({children}) => {
   return (
     <Text
-      style={{
-        fontSize: 22,
-        fontFamily: 'monospace',
-        color: '#f0f8ff',
-      }}>
+      style={[
+        Platform.OS === 'android'
+          ? {
+              fontSize: 22,
+              fontFamily: 'monospace',
+              color: '#f0f8ff',
+            }
+          : {
+              fontSize: 22,
+              fontFamily: 'arial',
+            },
+      ]}>
       {children}
     </Text>
   );
